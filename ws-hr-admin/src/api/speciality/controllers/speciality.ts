d08f23bbcb62c853a4678156184e80d6cbd0c7be
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 12:24:10 +0300

    feat(admin): populate tasks in speciality.ts controller

--- ws-hr-admin/src/api/speciality/controllers/speciality.ts
+++ ws-hr-admin/src/api/speciality/controllers/speciality.ts
@@ -2,6 +2,19 @@
  * speciality controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::speciality.speciality');
+export default factories.createCoreController('api::speciality.speciality', ({ strapi }) => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    const populatedSpecialities = await strapi.entityService.findMany('api::speciality.speciality', {
+      ...ctx.query,
+      populate: {
+        tasks: true,
+      },
+    });
+
+    return { data: populatedSpecialities, meta };
+  },
+}));
