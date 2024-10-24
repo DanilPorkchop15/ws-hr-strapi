1ab4974d5745cbcbb7f6c3402b3c788bbb06c6bf
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-24 12:21:44 +0300

    feat(plugin-shared): rm unused code

--- ws-hr-strapi-plugin/admin/src/shared/model/services/appRoutes.ts
+++
@@ -1,6 +0,0 @@
-export class AppRoutes {
-  public static getHomeUrl = () => "/";
-
-  private static readonly _calculatePrefix = (prefix: string, withPrefix: boolean) =>
-    withPrefix ? prefix + "/" : prefix;
-}
