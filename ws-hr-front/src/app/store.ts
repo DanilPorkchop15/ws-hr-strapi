cf0dfac0c2e9b1a21891438bfbbcf48a7c16179f
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-25 17:17:19 +0300

    feat(client): add app store

---
+++ ws-hr-front/src/app/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { taskLinkApi } from "entities/taskLink";
+
+import { baseApi } from "../shared/api";
+
+export const store = configureStore({
+  reducer: {
+    [taskLinkApi.reducerPath]: taskLinkApi.reducer,
+
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
