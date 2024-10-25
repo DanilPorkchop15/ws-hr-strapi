bd0096540e6bb1bfed0c98fc3cbb6b397faf04e0
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-25 17:11:44 +0300

    feat(client): add app

---
+++ ws-hr-front/src/app/config.tsx
@@ -0,0 +1,33 @@
+import { Provider } from "react-redux";
+import { Outlet } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+import { AppRoutes } from "shared/model";
+
+import { Layout } from "../shared/ui/layout";
+
+import { store } from "./store";
+
+export const browserRouter = createBrowserRouter([
+  {
+    path: "*",
+    element: "Not Found 404",
+  },
+  {
+    path: AppRoutes.getHomeUrl(),
+    element: (
+      <Provider store={store}>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </Provider>
+    ),
+    children: [
+      { index: true, element: "Ссылка должна содержать uuid" },
+      {
+        path: AppRoutes.getTaskUrl(),
+        lazy: async () => import("../pages/task"),
+      },
+    ],
+  },
+]);
---
+++ ws-hr-front/src/app/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from "react-router";
+
+import "./ui/index.css";
+
+import { browserRouter } from "./config";
+
+export function mountApp() {
+  const root = document.getElementById("root");
+  if (!root) throw new Error("root not found");
+  
+  ReactDOM.createRoot(root).render(
+    <RouterProvider fallbackElement="loading.." router={browserRouter} />
+  );
+}
