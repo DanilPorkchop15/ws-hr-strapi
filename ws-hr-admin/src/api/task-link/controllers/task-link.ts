b2c5ce7302e0d9d1dcd0e616116ef29d1d0fde44
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-25 11:37:48 +0300

    feat(admin): add custom controller

--- ws-hr-admin/src/api/task-link/controllers/task-link.ts
+++ ws-hr-admin/src/api/task-link/controllers/task-link.ts
@@ -2,6 +2,46 @@
  * task-link controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::task-link.task-link');
+export default factories.createCoreController(
+  "api::task-link.task-link",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const taskLink = await strapi.db
+        .query("api::task-link.task-link")
+        .findOne({
+          where: { uuid: id, isValid: true },
+          populate: ["task"],
+        });
+
+      if (!taskLink) {
+        return ctx.badRequest("Ссылка недействительна или уже использована");
+      }
+
+      return taskLink;
+    },
+
+    async update(ctx) {
+      const { id } = ctx.params;
+
+      const taskLink = await strapi.db
+        .query("api::task-link.task-link")
+        .findOne({
+          where: { uuid: id },
+          populate: ["task"],
+        });
+
+      if (!taskLink) {
+        return ctx.badRequest("Ссылка недействительна или уже использована");
+      }
+
+      return await strapi.db.query("api::task-link.task-link").update({
+        where: { uuid: id },
+        data: { isValid: false },
+      });
+    },
+  }),
+);
