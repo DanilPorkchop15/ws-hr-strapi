32c83846fb49a5ddddff386b7ee9c7f54051a334
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-25 17:11:03 +0300

    feat(client): add link api

---
+++ ws-hr-front/src/entities/taskLink/api/gateway.ts
@@ -0,0 +1,14 @@
+import { baseApi } from 'shared/api';
+import { ApiRoutes } from "shared/model";
+
+import type { TaskLink } from '../interfaces';
+
+export const taskLinkApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    getTaskLink: build.query<TaskLink, string>(
+      {
+        query: (uuid) => ApiRoutes.getTaskLink(false, uuid)
+      }
+    ),
+  }),
+});
