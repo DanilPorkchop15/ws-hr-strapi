670f1b30432572eb98808b30a5b9379b407e7794
Danil Kostin <d.kostin@worksolutions.ru>
2024-10-25 17:20:08 +0300

    fix(admin): fix controller logic

--- ws-hr-admin/src/api/task-link/controllers/task-link.ts
+++ ws-hr-admin/src/api/task-link/controllers/task-link.ts
@@ -3,6 +3,7 @@
  */
 
 import { factories } from "@strapi/strapi";
+import { v4 as uuidv4 } from "uuid";
 
 export default factories.createCoreController(
   "api::task-link.task-link",
@@ -21,27 +22,27 @@ export default factories.createCoreController(
         return ctx.badRequest("Ссылка недействительна или уже использована");
       }
 
+      await strapi.entityService.update("api::task-link.task-link", taskLink.id, {
+        data: {
+          isValid: false
+        }
+      })
+
       return taskLink;
     },
+    async create(ctx) {
+      const uuid = uuidv4();
+      const { data } = ctx.request.body;
+      const result = await strapi.entityService.create("api::task-link.task-link", {
+        data : {
+          ...data,
+          uuid,
+          isValid: true
+        }
+      })
+      const sanitizedResults = await this.sanitizeOutput(result, ctx);
 
-    async update(ctx) {
-      const { id } = ctx.params;
-
-      const taskLink = await strapi.db
-        .query("api::task-link.task-link")
-        .findOne({
-          where: { uuid: id },
-          populate: ["task"],
-        });
-
-      if (!taskLink) {
-        return ctx.badRequest("Ссылка недействительна или уже использована");
-      }
-
-      return await strapi.db.query("api::task-link.task-link").update({
-        where: { uuid: id },
-        data: { isValid: false },
-      });
-    },
+      return this.transformResponse(sanitizedResults);
+    }
   }),
 );
